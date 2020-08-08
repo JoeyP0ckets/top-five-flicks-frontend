8860a1430c44891e4b874eaf151fe51d21c92645
github username <github email address>
2020-08-08 11:55:29 -0400

    basic setup for Nav Profile and ContentPane

---
+++ src/components/ContentPane.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from './Profile'
+
+const ContentPane = () => {
+  return(
+    <div>
+      I'm the Content Pane
+      <Profile />
+    </div>
+  )
+}
+
+export default ContentPane
